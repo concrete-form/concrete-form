093e7a1bf880de753f65b9c47ab80a95bea519c2
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-09 16:24:12 -0500

    Added tests for defaultTranslator

---
+++ src/translation/defaultTranslator.test.ts
@@ -0,0 +1,46 @@
+import defaultTranslator from './defaultTranslator'
+import en from '../locales/en'
+import fr from '../locales/fr'
+
+describe('defaultTranslator', () => {
+  it('supports english', () => {
+    const translator = defaultTranslator('en')
+    expect(translator({ key: 'default' })).toBe(en.default)
+  })
+
+  it('supports french', () => {
+    const translator = defaultTranslator('fr')
+    expect(translator({ key: 'default' })).toBe(fr.default)
+  })
+
+  it('use english by default', () => {
+    const translator = defaultTranslator()
+    expect(translator({ key: 'default' })).toBe(en.default)
+  })
+
+  it('fallback to english for unknown languages', () => {
+    const translator = defaultTranslator('xy')
+    expect(translator({ key: 'default' })).toBe(en.default)
+  })
+
+  it('do not translate strings', () => {
+    const translator = defaultTranslator()
+    expect(translator('default')).toBe('default')
+  })
+
+  it('returns the key for unknown translation', () => {
+    const translator = defaultTranslator()
+    expect(translator({ key: 'test-foo' })).toBe('test-foo')
+  })
+
+  it('apply values', () => {
+    const translator = defaultTranslator()
+    expect(translator({ key: 'length', values: { length: '666' } })).toMatch(/666/)
+  })
+
+  it('removed unfilled variables', () => {
+    const translator = defaultTranslator()
+    expect(en.length).toMatch(/{{length}}/)
+    expect(translator({ key: 'length' })).not.toMatch(/{{length}}/)
+  })
+})
