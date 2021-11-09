d910f3ad479785784958ea5586570a7a7d03ea5a
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-09 16:26:05 -0500

    moved CustomizableLayout from types to util

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,3 @@
 export * from './types'
-export { default as CustomizableLayout } from './CustomizableLayout'
 export { default as AvailableControls } from './Controls.enum'
 export { AvailableLayout } from './Layout.enum'
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,2 +1,3 @@
 export * from './options'
 export * from './events'
+export { default as CustomizableLayout } from './CustomizableLayout'
