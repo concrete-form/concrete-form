82f9a79a638e6c317ee0ea1e4bd5c12e3ae789bb
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-12 09:22:29 -0500

    Included new config in config hook test

--- src/hooks/useConcreteFormConfig/useConcreteFormConfig.test.tsx
+++ src/hooks/useConcreteFormConfig/useConcreteFormConfig.test.tsx
@@ -6,7 +6,7 @@ const CustomLayout: React.FC = () => <div>...</div>
 
 describe('useConcreteFormConfig', () => {
   it('returns the configs', () => {
-    const concreteFormConfig = { language: 'fr', layout: { [Layout.Control]: CustomLayout } }
+    const concreteFormConfig = { language: 'fr', layout: { [Layout.Control]: CustomLayout }, disableWhileSubmitting: false }
     const result = renderHook(useConcreteFormConfig, { concreteFormConfig }).result.current as {}
     expect(result).toEqual(concreteFormConfig)
   })
