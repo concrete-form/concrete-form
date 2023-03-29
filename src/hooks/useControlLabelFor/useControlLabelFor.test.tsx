a9d5514efe0143276f86d282699abaa9e0c8cbb7
Kevin Gilbert <kevin@kegisiteweb.com>
2023-03-29 14:04:21 -0400

    Fixed GH issue#1 ; useControlLabelFor() now always return a value when a control with a prop name is provided

--- src/hooks/useControlLabelFor/useControlLabelFor.test.tsx
+++ src/hooks/useControlLabelFor/useControlLabelFor.test.tsx
@@ -3,43 +3,22 @@ import useControlLabelFor from './useControlLabelFor'
 
 type FakeControlProps = { name?: string }
 
-const Autocomplete: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
-const CustomControl: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
-const DateTime: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
 const Input: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
-const Select: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
-const Slider: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
-const Textarea: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
-
-const supportedControls = [Autocomplete, CustomControl, DateTime, Input, Select, Slider, Textarea]
-
-const Radio: React.FC<FakeControlProps> = ({ name }) => <input name={name} />
 
 describe('useControlLabelFor', () => {
-  describe('supported controls', () => {
-    supportedControls.forEach(Control => {
-      it(`supports ${Control.name}`, () => {
-        const name = `test-${Control.name.toLowerCase()}`
-        const hook = renderHook(() => useControlLabelFor(<Control name={name} />))
-        expect(hook.result.current).toMatch(new RegExp(`.+-${name}$`))
-      })
-    })
+  it('supports components with name prop', () => {
+    const name = 'foo-bar-baz'
+    const { result } = renderHook(() => useControlLabelFor(<Input name={name} />))
+    expect(result.current).toMatch(new RegExp(`.+-${name}$`))
   })
 
-  describe('not supported controls', () => {
-    it('ignores component with no type', () => {
-      const hook = renderHook(() => useControlLabelFor('invalid'))
-      expect(hook.result.current).toBeUndefined()
-    })
+  it('ignores invalid component', () => {
+    const { result } = renderHook(() => useControlLabelFor('invalid'))
+    expect(result.current).toBeUndefined()
+  })
 
-    it('ignores component with no name', () => {
-      const hook = renderHook(() => useControlLabelFor(<Input />))
-      expect(hook.result.current).toBeUndefined()
-    })
-
-    it('ignores controls that aren\'t supported', () => {
-      const hook = renderHook(() => useControlLabelFor(<Radio name="test" />))
-      expect(hook.result.current).toBeUndefined()
-    })
+  it('ignores component with no name', () => {
+    const { result } = renderHook(() => useControlLabelFor(<Input />))
+    expect(result.current).toBeUndefined()
   })
 })
--- src/hooks/useControlLabelFor/useControlLabelFor.ts
+++ src/hooks/useControlLabelFor/useControlLabelFor.ts
@@ -2,22 +2,9 @@ import useConcreteFormId from '../useConcreteFormId'
 
 const useControlLabelFor = (control: any) => {
   const formId = useConcreteFormId()
-  const controlType = control?.type?.name ?? ''
   const controlName = control?.props?.name
 
-  if (!controlType || !controlName) {
-    return
-  }
-
-  if (![
-    'Autocomplete',
-    'CustomControl',
-    'DateTime',
-    'Input',
-    'Select',
-    'Slider',
-    'Textarea',
-  ].includes(controlType)) {
+  if (!controlName) {
     return
   }
 
